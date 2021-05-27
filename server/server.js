f0ba08af9279b82101c3f9db00d960fb76fb60e7
Alex Santini <47783526+AlexSantini10@users.noreply.github.com>
2021-05-27 23:13:23 +0200

    [update] Update

---
+++ server/server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const http = require('http');
+const path = require('path')
+const socketio = require('socket.io')
+const formatMessage = require('./utils/messages');
+const {userJoin, getCurrentUser, userLeave, getRoomUsers} = require('./utils/users');
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+const server = http.createServer(app)
+
+const io = socketio(server);
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+io.on('connection', (socket) => {
+    socket.on('joinRoom', ({username, room}) => {
+        const user = userJoin(socket.id, username, room);
+
+        socket.join(user.room);
+
+        socket.emit('message', formatMessage('AdminBot', 'Benvenuto'));
+
+        socket.broadcast.to(user.room).emit('message', formatMessage('AdminBot', user.username + ' è entrato nella chat'));
+    
+        io.to(user.room).emit('roomUsers', {room: user.room, users: getRoomUsers(user.room)});
+    });
+
+    socket.on('disconnect', () => {
+        const user = userLeave(socket.id);
+
+        if(user){
+            io.to(user.room).emit('message', formatMessage('AdminBot', user.username + ' si è disconnesso'));
+
+            io.to(user.room).emit('roomUsers', {room: user.room, users: getRoomUsers(user.room)});
+        }
+    })
+
+    socket.on('chatMessage', (msg) => {
+        const user = getCurrentUser(socket.id);
+
+        io.to(user.room).emit('message', formatMessage(user.username, msg));
+    })
+});
+
+
+
+server.listen(port, () => {
+    console.log('started on port: ' + port);
+});
